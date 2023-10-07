1d6b155781e3705f98f92a506ac33466d848b6c6
awesomeaj5 <43348940+awesomeaj5@users.noreply.github.com>
2023-10-06 18:17:23 -0700

    Added animations and transitions
    Still having trouble transitioning back to the original animation

--- Assets/Scripts/Skater.cs
+++ Assets/Scripts/Skater.cs
@@ -1,15 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Skater : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 0.05f;
+    Animator myAnimator;
+    Rigidbody2D myRigidBody;
+    CapsuleCollider2D myCapsuleCollider;
+    bool isGrounded = false;
+
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.03f, 0, 0);
+        // Check for ground contact in the Update method
+        isGrounded = myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        Debug.Log("isGrounded = " + isGrounded);
+
+        transform.Translate(speed, 0, 0);
+
+        // Ollie logic
+        if (isGrounded)
+        {
+            myAnimator.SetBool("isOllie", false);
+            myAnimator.SetBool("isRolling", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isRolling", false);
+        }
+    }
+
+    void OnOllie(InputValue value)
+    {
+        // Perform ollie only if the skater is grounded
+        if (isGrounded)
+        {
+            myAnimator.SetBool("isOllie", true);
+            myRigidBody.velocity += new Vector2(0f, 4f);
+            Debug.Log("Ollie animation triggered");
+        }
+    }
+
+    void OnKickflip(InputValue value)
+    {
+        Debug.Log("Doing kickflip");
+    }
+
+    void OnHeelflip(InputValue value)
+    {
+        Debug.Log("Doing heelflip now");
+    }
+
+    void OnGrab(InputValue value)
+    {
+        Debug.Log("Doing grab now");
     }
 }
